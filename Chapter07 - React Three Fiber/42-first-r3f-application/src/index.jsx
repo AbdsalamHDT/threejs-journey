cd096e0e9549cc523b62ab1ac6c12f04275cff05
AbdsalamHDT <57990750+AbdsalamHDT@users.noreply.github.com>
2023-05-09 13:30:14 +0100

    First R3F Application Done ✅

--- Chapter07 - React Three Fiber/42-first-r3f-application/src/index.jsx
+++ Chapter07 - React Three Fiber/42-first-r3f-application/src/index.jsx
@@ -6,7 +6,17 @@ import Experience from './Experience'
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-    <Canvas>
+    <Canvas
+        gl={ {
+            antialias: true
+        } }
+        camera={ {
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [ 3, 2, 6 ]
+        } }
+    >
         <Experience />
     </Canvas>
 )
